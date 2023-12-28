636a5348c47b6c6089a34334b912af5d567e948a
Speykious <speykious@gmail.com>
2023-12-28 19:14:14 +0100

    cargo fmt

--- src/opengl.rs
+++ src/opengl.rs
@@ -3,9 +3,7 @@ use std::error::Error;
 use std::ffi::CString;
 use std::num::NonZeroU32;
 
-use glutin::context::{
-    ContextApi, ContextAttributesBuilder, NotCurrentGlContext, PossiblyCurrentContext, Version,
-};
+use glutin::context::{ContextApi, ContextAttributesBuilder, NotCurrentGlContext, PossiblyCurrentContext, Version};
 use glutin::display::{Display, GetGlDisplay};
 use glutin::prelude::{GlConfig, GlDisplay};
 use glutin::surface::{Surface, SurfaceAttributesBuilder, WindowSurface};
@@ -15,71 +13,71 @@ use winit::event_loop::EventLoop;
 use winit::window::Window;
 
 pub struct OpenglCtx {
-    pub gl_ctx: PossiblyCurrentContext,
-    pub gl_surface: Surface<WindowSurface>,
-    pub gl_display: Display,
-    pub window: Window,
-    pub events: EventLoop<()>,
+	pub gl_ctx: PossiblyCurrentContext,
+	pub gl_surface: Surface<WindowSurface>,
+	pub gl_display: Display,
+	pub window: Window,
+	pub events: EventLoop<()>,
 }
 
 pub fn create_opengl_window(width: u32, height: u32) -> Result<OpenglCtx, Box<dyn Error>> {
-    if cfg!(target_os = "linux") {
-        // disables vsync sometimes on x11
-        if env::var("vblank_mode").is_err() {
-            env::set_var("vblank_mode", "0");
-        }
-    }
+	if cfg!(target_os = "linux") {
+		// disables vsync sometimes on x11
+		if env::var("vblank_mode").is_err() {
+			env::set_var("vblank_mode", "0");
+		}
+	}
 
-    let events = winit::event_loop::EventLoop::new()?;
+	let events = winit::event_loop::EventLoop::new()?;
 
-    let window_builder = winit::window::WindowBuilder::new()
-        .with_transparent(true)
-        .with_resizable(true)
-        .with_inner_size(winit::dpi::PhysicalSize::new(width, height))
-        .with_title("OwOverlay");
+	let window_builder = winit::window::WindowBuilder::new()
+		.with_transparent(true)
+		.with_resizable(true)
+		.with_inner_size(winit::dpi::PhysicalSize::new(width, height))
+		.with_title("OwOverlay");
 
-    let (window, gl_config) = glutin_winit::DisplayBuilder::new()
-        .with_preference(ApiPreference::FallbackEgl)
-        .with_window_builder(Some(window_builder))
-        .build(&events, <_>::default(), |configs| {
-            configs
-                .filter(|c| c.srgb_capable())
-                .max_by_key(|c| c.num_samples())
-                .unwrap()
-        })?;
+	let (window, gl_config) = glutin_winit::DisplayBuilder::new()
+		.with_preference(ApiPreference::FallbackEgl)
+		.with_window_builder(Some(window_builder))
+		.build(&events, <_>::default(), |configs| {
+			configs
+				.filter(|c| c.srgb_capable())
+				.max_by_key(|c| c.num_samples())
+				.unwrap()
+		})?;
 
-    let window = window.unwrap(); // set in display builder
-    let raw_window_handle = window.raw_window_handle();
-    let gl_display = gl_config.display();
+	let window = window.unwrap(); // set in display builder
 
-    let context_attributes = ContextAttributesBuilder::new()
-        .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 1))))
-        .with_profile(glutin::context::GlProfile::Core)
-        .build(Some(raw_window_handle));
+	let raw_window_handle = window.raw_window_handle();
+	let gl_display = gl_config.display();
 
-    let dimensions = window.inner_size();
+	let context_attributes = ContextAttributesBuilder::new()
+		.with_context_api(ContextApi::OpenGl(Some(Version::new(3, 1))))
+		.with_profile(glutin::context::GlProfile::Core)
+		.build(Some(raw_window_handle));
 
-    let (gl_surface, gl_ctx) = {
-        let attrs = SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new().build(
-            raw_window_handle,
-            NonZeroU32::new(dimensions.width).unwrap(),
-            NonZeroU32::new(dimensions.height).unwrap(),
-        );
+	let dimensions = window.inner_size();
 
-        let surface = unsafe { gl_display.create_window_surface(&gl_config, &attrs)? };
-        let context = unsafe { gl_display.create_context(&gl_config, &context_attributes)? }
-            .make_current(&surface)?;
-        (surface, context)
-    };
+	let (gl_surface, gl_ctx) = {
+		let attrs = SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new().build(
+			raw_window_handle,
+			NonZeroU32::new(dimensions.width).unwrap(),
+			NonZeroU32::new(dimensions.height).unwrap(),
+		);
 
-    // Load the OpenGL function pointers
-    gl::load_with(|symbol| gl_display.get_proc_address(&CString::new(symbol).unwrap()) as *const _);
+		let surface = unsafe { gl_display.create_window_surface(&gl_config, &attrs)? };
+		let context = unsafe { gl_display.create_context(&gl_config, &context_attributes)? }.make_current(&surface)?;
+		(surface, context)
+	};
 
-    Ok(OpenglCtx {
-        gl_ctx,
-        gl_surface,
-        gl_display,
-        window,
-        events,
-    })
+	// Load the OpenGL function pointers
+	gl::load_with(|symbol| gl_display.get_proc_address(&CString::new(symbol).unwrap()) as *const _);
+
+	Ok(OpenglCtx {
+		gl_ctx,
+		gl_surface,
+		gl_display,
+		window,
+		events,
+	})
 }
--- src/window.rs
+++ src/window.rs
@@ -9,70 +9,70 @@ use winit::event::{ElementState, Event, KeyEvent, WindowEvent};
 use winit::event_loop::ControlFlow;
 use winit::keyboard::{KeyCode, PhysicalKey};
 
-use crate::Scene;
 use crate::opengl::{create_opengl_window, OpenglCtx};
+use crate::Scene;
 
 pub fn spawn_window(scene: &mut impl Scene) -> Result<(), Box<dyn Error>> {
-    let (width, height) = (500, 800);
+	let (width, height) = (500, 800);
 
-    let OpenglCtx {
-        gl_ctx,
-        gl_surface,
-        gl_display,
-        events,
-        window,
-    } = create_opengl_window(width, height)?;
+	let OpenglCtx {
+		gl_ctx,
+		gl_surface,
+		gl_display,
+		events,
+		window,
+	} = create_opengl_window(width, height)?;
 
-    let mut drawer = OpenglDrawer::new(width, height, 1.);
-    let mut viewport = vec2(width as f32, height as f32);
+	let mut drawer = OpenglDrawer::new(width, height, 1.);
+	let mut viewport = vec2(width as f32, height as f32);
 
-    // Event loop
-    events.run(move |event, elwt| {
-        // They need to be present
-        let _gl_display = &gl_display;
-        let _window = &window;
+	// Event loop
+	events.run(move |event, elwt| {
+		// They need to be present
+		let _gl_display = &gl_display;
+		let _window = &window;
 
-        elwt.set_control_flow(ControlFlow::Wait);
+		elwt.set_control_flow(ControlFlow::Wait);
 
-        match event {
-            Event::WindowEvent { ref event, .. } => match event {
-                WindowEvent::RedrawRequested => {
+		match event {
+			Event::WindowEvent { ref event, .. } => match event {
+				WindowEvent::RedrawRequested => {
 					scene.update();
-                    scene.draw(viewport, &mut drawer);
+					scene.draw(viewport, &mut drawer);
 
-                    gl_surface.swap_buffers(&gl_ctx).unwrap();
-                    window.request_redraw();
-                }
-                WindowEvent::Resized(physical_size) => {
-                    // Handle window resizing
-                    viewport = vec2(physical_size.width as f32, physical_size.height as f32);
-                    drawer.resize(viewport, 1.);
+					gl_surface.swap_buffers(&gl_ctx).unwrap();
+					window.request_redraw();
+				}
+				WindowEvent::Resized(physical_size) => {
+					// Handle window resizing
+					viewport = vec2(physical_size.width as f32, physical_size.height as f32);
+					drawer.resize(viewport, 1.);
 
-                    gl_surface.resize(
-                        &gl_ctx,
-                        NonZeroU32::new(physical_size.width).unwrap(),
-                        NonZeroU32::new(physical_size.height).unwrap(),
-                    );
-                    window.request_redraw();
-                }
-                WindowEvent::CloseRequested => elwt.exit(),
-                WindowEvent::KeyboardInput {
-                    event:
-                        KeyEvent {
-                            physical_key: PhysicalKey::Code(KeyCode::Escape),
-                            state: ElementState::Pressed,
-                            ..
-                        },
-                    ..
-                } => elwt.exit(),
-                _ => (),
-            },
-            Event::AboutToWait => {
-                window.request_redraw();
-            }
-            _ => (),
-        }
-    })?;
+					gl_surface.resize(
+						&gl_ctx,
+						NonZeroU32::new(physical_size.width).unwrap(),
+						NonZeroU32::new(physical_size.height).unwrap(),
+					);
+					window.request_redraw();
+				}
+				WindowEvent::CloseRequested => elwt.exit(),
+				WindowEvent::KeyboardInput {
+					event:
+						KeyEvent {
+							physical_key: PhysicalKey::Code(KeyCode::Escape),
+							state: ElementState::Pressed,
+							..
+						},
+					..
+				} => elwt.exit(),
+				_ => (),
+			},
+			Event::AboutToWait => {
+				window.request_redraw();
+			}
+			_ => (),
+		}
+	})?;
 
-    Ok(())
+	Ok(())
 }
